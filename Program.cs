07fc73adaa6481499813bbae187aed33614dd9dd
svenbatalla-llw <69117565+svenbatalla-llw@users.noreply.github.com>
2021-03-13 13:32:23 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace repo02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!")
         }
     }
 }
